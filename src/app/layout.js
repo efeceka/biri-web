2fd9a668de8457b23e438bb03d6d740ab07754b0
efeceka <efecetinkaya@hotmail.com>
2025-08-17 18:59:12 +0200

    fix.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,38 +1,5 @@
-import { Inter, Roboto, Montserrat, Nunito_Sans} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
-
-
-// TT Norms yerine Inter
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
-
-// Codec Pro yerine Montserrat
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-  variable: "--font-montserrat",
-});
-
-const nunito = Nunito_Sans({
-  subsets: ["latin"],
-  weight: ["400", "600", "700", "800"], // başlık için bold kesitleri aç
-  variable: "--font-nunito",
-});
-
-// Roboto zaten mevcut
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-roboto",
-});
-
-export const metadata = {
-  title: "Biri Teknoloji",
-  description: "Geleceğin teknolojileri, hayatınızı bugün kolaylaştırsın!",
-};
 
 export default function RootLayout({ children }) {
   return (
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,4 @@
 import Hero from "@/components/Hero";
-import Header from "@/components/layout/Header";
-import Image from "next/image";
-import Hakkimizda from "./hakkimizda/page";
 
 
 export default function Home() {
