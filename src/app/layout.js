104ec49d9ee00d1a2fda4474b33c4eed1eb13116
efeceka <efecetinkaya@hotmail.com>
2025-08-17 18:50:46 +0200

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="tr"
-      className={`${inter.variable} ${roboto.variable} ${montserrat.variable} ${nunito.variable} `}
+      className=""
     >
       <body>
         <Header/>
