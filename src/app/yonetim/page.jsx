db4c08d381bb4d8dbb3b32993b9abc9ca3b3c3e7
efeceka <efecetinkaya@hotmail.com>
2025-08-18 02:53:21 +0200

    fix

--- src/app/yonetim/page.jsx
+++ src/app/yonetim/page.jsx
@@ -105,7 +105,7 @@ export default function YonetimPage() {
     h-auto object-contain opacity-95 select-none pointer-events-none
   "
 />
-    <div className="px-4 text-[10px] lg:text-[12px] text-center lg:py-2">
+    <div className="px-4 text-[10px] lg:text-[12px] text-center lg:py-2 hidden md:block">
       <p>Ticari Unvan: Biri İleri Teknoloji Bilişim Sanayi ve Ticaret A.Ş.</p>
       <p>Ticaret Sicil Numarası: 78086</p>
       <p>Mersis Numarası: 0177081335200001</p>
